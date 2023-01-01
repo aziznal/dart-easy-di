944a1405bbba444226966b85f53527d73b76016c
Aziz <aziznal.dev@gmail.com>
2023-01-01 11:24:56 +0300

    Fix failing tests

--- package_a/test/package_a_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:package_a/package_a.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- package_c/test/package_c_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:package_c/package_c.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
